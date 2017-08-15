37b5d7f771bf3e13153a2d3dfed29eaf543fcf94
takao-s <takao.sekiguchi@gmail.com>
2017-08-15 23:06:13 +0900

    beacon検知がONからOFF、OFFからONになった時に画面制御が変化しないバグの修正

--- www/js/memory.js
+++ www/js/memory.js
@@ -11,27 +11,19 @@ bstrap.controller('MemoryController', function($scope, $interval){
     }
 
     $scope.startInterval = function() {
-        console.log("sssssssssss");
-        console.log(app.stop);
         if ( angular.isDefined(app.stop) ) { return; }
+        console.log("startInterval");
         
         app.stop = $interval(function() {
-            console.log("hogehoge:" + JSON.stringify($scope.beacons));
-            if (!$scope._detected) {
-                $scope.stopInterval();
-            }
+            console.log("beacons:" + JSON.stringify($scope.beacons));
         }, 2000);
     };
 
-    $scope.stopInterval = function() {
-        if (angular.isDefined(app.stop)) {
-          $interval.cancel(app.stop);
-          app.stop = undefined;
-        }
-    };
-
     $scope.$on('$destroy', function() {
-        $scope.stopInterval();
+        if (angular.isDefined(app.stop)) {
+            $interval.cancel(app.stop);
+            app.stop = undefined;
+        }
     });
 
     ons.ready(function(){
@@ -49,17 +41,7 @@ bstrap.controller('MemoryController', function($scope, $interval){
         
         var _time = Date.now();
         delegate.didRangeBeaconsInRegion = function (pluginResult) {
-            // console.log('[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
-            // if (Date.now() - _time > 5000 && pluginResult.beacons.length) {
-            //     console.log("hogehoge:" + JSON.stringify(pluginResult.beacons));
-            //     $scope.beacons = pluginResult.beacons;
-            //     // document.getElementById("beacon").innerHTML = JSON.stringify(pluginResult.beacons);
-            //     $scope._detected= true;
-            //     _time = Date.now();
-            // }
-            if (pluginResult.beacons.length) {
-                $scope._detected = true;
-            }
+            $scope._detected = pluginResult.beacons.length ? true : false;
             $scope.beacons = pluginResult.beacons;
         };
     
