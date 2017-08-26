bf066962f6743a8bf76e5465cb2aa40df4069e37
takao-s <takao.sekiguchi@gmail.com>
2017-08-26 11:40:40 +0900

    タイムライン表示処理の修正

--- www/js/timeline.js
+++ www/js/timeline.js
@@ -1,3 +1,93 @@
-bstrap.controller('TimelineController', function($scope) {
+bstrap.controller('TimelineController', function($scope, $interval, BeaconTimelineService) {
+    var timeoutId;
+
     $scope.beacon = app.beacon;
+
+    $scope.toots = [];
+
+    var successCallback = function(data) {
+        $scope.toots = data;
+    };
+    var successAppendCallback = function(data) {
+        $scope.toots = data.concat($scope.toots);
+    };
+    var errorCallback = function(msg) {
+        $scope.alert = {msg: msg};
+    };
+    BeaconTimelineService.init(app.beacon)
+    .then(successCallback, errorCallback);
+
+
+    $scope.startInterval = function() {
+        if ( angular.isDefined(timeoutId) ) { return; }
+        console.log("startInterval");
+        
+        timeoutId = $interval(function() {
+            BeaconTimelineService.append(app.beacon, $scope.toots[0].id)
+            .then(successAppendCallback, errorCallback);
+
+        }, 1000);
+    };
+
+    $scope.$on('$destroy', function() {
+        if (angular.isDefined(timeoutId)) {
+            $interval.cancel(timeoutId);
+            timeoutId = undefined;
+        }
+    });
+
+    ons.ready(function(){
+        if (!app.demo) { setUp(); }
+        $scope.startInterval();
+    });
+});
+
+bstrap.factory('BeaconTimelineService', function($q, $http){
+    var returnReq = function(beacon, toot_id) {
+        var req = {
+            method: 'GET',
+            // url: beacon.beadonURL + '/api/v1/timelines/public',
+            url: 'https://devcampndd.com//api/v1/timelines/public?limit=3',
+            headers: {
+                // Authorization: 'Bearer ' + beacon.accessToken
+                Authorization: 'Bearer 46e7eae67ebf42fcec15105758b39f671b00e1a31bcc50b8f174fb4226a0edae'
+            }
+        };
+        if (toot_id) {
+            req.url = req.url + "&since_id=" + (toot_id);
+        }
+        return req;
+    };
+    return {
+        init: function(beacon) {
+            var req = returnReq(beacon);
+            var deferred = $q.defer();
+            $http(req)
+            .then(function (response) {
+                console.log("status:" + response.status);
+                deferred.resolve(response.data);                
+            })
+            .catch(function(response) {
+                console.log("status:" + response.status);
+                console.error('Error occurred:', response.status, response.data);
+                deferred.reject('Error occurred:', response.status, response.data);
+            });
+            return deferred.promise;
+        },
+        append: function(beacon, toot_id) {
+            var req = returnReq(beacon, toot_id);
+            var deferred = $q.defer();
+            $http(req)
+            .then(function (response) {
+                console.log("status:" + response.status);
+                deferred.resolve(response.data);                
+            })
+            .catch(function(response) {
+                console.log("status:" + response.status);
+                console.error('Error occurred:', response.status, response.data);
+                deferred.reject('Error occurred:', response.status, response.data);
+            });
+            return deferred.promise;
+        }
+    };
 });
